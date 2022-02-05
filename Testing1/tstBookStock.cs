26f108d037222120ee04e2ffa14db596175a3006
Fahim Ahmed <P2595607@my365.dmu.ac.uk>
2022-02-05 16:29:38 +0000

    Added testing for BookStock class

---
+++ Testing1/tstBookStock.cs
@@ -0,0 +1,19 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing1
+{
+    [TestClass]
+    public class tstBookStock
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create a new instance of the class required
+            clsBookStock ABookStock = new clsBookStock();
+            //test to see if it exists
+            Assert.IsNotNull(ABookStock);
+        }
+    }
+}

b2e7c1cd60fb6220976aab36b60cf836ed4878e9
FabianB2002 <P2597742@my365.dmu.ac.uk>
2022-02-16 20:28:14 +0000

    Added testing for each attribute in my table

--- ClassLibrary/clsCustomer.cs
+++ ClassLibrary/clsCustomer.cs
@@ -1,6 +1,14 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsCustomer
     {
+        public int CustomerID { get; set; }
+        public string CustomerFirstName { get; set; }
+        public string CustomerLastName { get; set; }
+        public string CustomerAddress { get; set; }
+        public DateTime CustomerAccCreated { get; set; }
+        public bool ExistingAcc { get; set; }
     }
 }
--- Testing2/tstCustomer.cs
+++ Testing2/tstCustomer.cs
@@ -15,5 +15,65 @@ namespace Testing2
             //test to see that it exists
             Assert.IsNotNull(TheCustomer);
         }
+
+        [TestMethod]
+
+        public void CustomerIDPropertyOK()
+        {
+            clsCustomer TheCustomer = new clsCustomer();
+            int TestData = 1;
+            TheCustomer.CustomerID = TestData;
+            Assert.AreEqual(TheCustomer.CustomerID, TestData);
+
+        }
+
+        [TestMethod]
+        public void CustomerFirstNamePropertyOK()
+        {
+            clsCustomer TheCustomer = new clsCustomer();
+            string TestData = "Joe";
+            TheCustomer.CustomerFirstName = TestData;
+            Assert.AreEqual(TheCustomer.CustomerFirstName, TestData);
+
+        }
+
+        [TestMethod]
+        public void CustomerLastNamePropertyOK()
+        {
+            clsCustomer TheCustomer = new clsCustomer();
+            string TestData = "Bloggs";
+            TheCustomer.CustomerLastName = TestData;
+            Assert.AreEqual(TheCustomer.CustomerLastName, TestData);
+        }
+
+        [TestMethod]
+
+        public void CustomerAddressPropertyOK()
+        {
+            clsCustomer TheCustomer = new clsCustomer();
+            string TestData = "33 Tor Church Road, Torquay,TQ2 5UP";
+            TheCustomer.CustomerAddress = TestData;
+            Assert.AreEqual(TheCustomer.CustomerAddress, TestData);
+        }
+
+        [TestMethod]
+
+        public void CustomerAccCreated()
+        {
+            clsCustomer TheCustomer = new clsCustomer();
+            DateTime TestData = DateTime.Now.Date;
+            TheCustomer.CustomerAccCreated = TestData;
+            Assert.AreEqual(TheCustomer.CustomerAccCreated, TestData);
+        }
+
+        [TestMethod]
+
+        public void ExistingAccPropertyOK()
+        {
+            clsCustomer TheCustomer = new clsCustomer();
+            Boolean TestData = true;
+            TheCustomer.ExistingAcc = TestData;
+            Assert.AreEqual(TheCustomer.ExistingAcc, TestData);
+        }
     }
 }

e238a5ae3a174ce44e723b3830e238be4e06469d
Dylan Wilson <P2600429@my365.dmu.ac.uk>
2022-03-03 16:16:02 +0000

    Created clcStaff and stsStaff Files as well as learnt how to use the github push and pull

---
+++ Testing3/tstStaff.cs
@@ -0,0 +1,17 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing3
+{
+    [TestClass]
+    public class tstStaff
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsStaff AStafff = new clsStaff();
+            Assert.IsNotNull(AStafff);
+        }
+    }
+}

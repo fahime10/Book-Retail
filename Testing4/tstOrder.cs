80f5a8f70cab8e1da441c6a0ebf6eb2aff58abb1
Harrison White <p2596843@my365.dmu.ac.uk>
2022-03-03 14:51:48 +0000

    Added properties to testing. Completed lab week 20.

--- Testing4/tstOrder.cs
+++ Testing4/tstOrder.cs
@@ -13,5 +13,63 @@ namespace Testing4
             clsOrder AnOrder = new clsOrder();
             Assert.IsNotNull(AnOrder);
         }
+
+        [TestMethod]
+        public void isStaffOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            Boolean TestData = true;
+            AnOrder.isStaff = TestData;
+            Assert.AreEqual(AnOrder.isStaff, TestData);
+
+        }
+
+        [TestMethod]
+        public void orderPlacedDateOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            DateTime TestData = DateTime.Now.Date;
+            AnOrder.DateAdded = TestData;
+            Assert.AreEqual(AnOrder.DateAdded, TestData);
+            
+        }
+
+        [TestMethod]
+        public void unitQuantityOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            int TestData = 1;
+            AnOrder.unitQuantity = TestData;
+            Assert.AreEqual(AnOrder.unitQuantity, TestData);
+        }
+
+        [TestMethod]
+        public void ShipAddressOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            string TestData = "54, highfield Street, Leicester, LE2 1AD";
+            AnOrder.ShipAddress = TestData;
+            Assert.AreEqual(AnOrder.ShipAddress, TestData);
+        }
+
+        [TestMethod]
+        public void totalPriceOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            double TestData = 12.23;
+            AnOrder.totalPrice = TestData;
+            Assert.AreEqual(AnOrder.totalPrice, TestData);
+
+        }
+
+        public void orderIDOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            int TestData = 1;
+            AnOrder.OrderID = TestData;
+            Assert.AreEqual(AnOrder.OrderID, TestData);
+        }
+        
+
     }
 }

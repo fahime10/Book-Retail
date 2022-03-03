73e2b9e423d0c72eed29f6d6752d07a411e84125
Dylan Wilson <P2600429@my365.dmu.ac.uk>
2022-03-03 20:01:44 +0000

    Created 1 test for each attribute in the Staff SQL Table.

--- ClassLibrary/clsStaff.cs
+++ ClassLibrary/clsStaff.cs
@@ -1,6 +1,14 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsStaff
     {
+        public bool StaffEmployed { get; set; }
+        public int StaffID { get; set; }
+        public DateTime StaffEndDate { get; set; }
+        public string StaffLastName { get; set; }
+        public string StaffFirstName { get; set; }
+        public double StaffSalary { get; set; }
     }
 }
--- Testing3/tstStaff.cs
+++ Testing3/tstStaff.cs
@@ -9,9 +9,76 @@ namespace Testing3
     {
         [TestMethod]
         public void InstanceOK()
-        {
+        {   //create an instance of the class we want to create
             clsStaff AStaff = new clsStaff();
+            //tests to see if it is not null
             Assert.IsNotNull(AStaff);
+        }
+        [TestMethod]
+        public void StaffEmployedOK()
+        {   //create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            //creates a boolean test data 
+            Boolean TrueData = true;
+            //assigns the data in the sql table equal to the variable above
+            AStaff.StaffEmployed = TrueData;
+            //checks to see if they are equal
+            Assert.AreEqual(AStaff.StaffEmployed, TrueData);
+        }
+        [TestMethod]
+        public void StaffSalaryOK()
+        {   //create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            //creates a double test data 
+            double TrueData = 20000;
+            //assigns the data in the sql table equal to the variable above
+            AStaff.StaffSalary = TrueData;
+            //checks to see if they are equal
+            Assert.AreEqual(AStaff.StaffSalary, TrueData);
+        }
+        [TestMethod]
+        public void StaffFirstNameOK()
+        {   //create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            //creates a string test data 
+            String TrueData = "Adam";
+            //assigns the data in the sql table equal to the variable above
+            AStaff.StaffFirstName = TrueData;
+            //checks to see if they are equal
+            Assert.AreEqual(AStaff.StaffFirstName, TrueData);
+        }
+        [TestMethod]
+        public void StaffLastNameOK()
+        {   //create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            //creates a string test data 
+            String TrueData = "Smith";
+            //assigns the data in the sql table equal to the variable above
+            AStaff.StaffLastName = TrueData;
+            //checks to see if they are equal
+            Assert.AreEqual(AStaff.StaffLastName, TrueData);
+        }
+        [TestMethod]
+        public void StaffEndDateOK()
+        {   //create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            //creates a DateTime test data 
+            DateTime TrueData = DateTime.Now.Date;
+            //assigns the data in the sql table equal to the variable above
+            AStaff.StaffEndDate = TrueData;
+            //checks to see if they are equal
+            Assert.AreEqual(AStaff.StaffEndDate, TrueData);
+        }
+        [TestMethod]
+        public void StaffIDOK()
+        {   //create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            //creates a Integer test data 
+            Int32 TrueData = 5;
+            //assigns the data in the sql table equal to the variable above
+            AStaff.StaffID = TrueData;
+            //checks to see if they are equal
+            Assert.AreEqual(AStaff.StaffID, TrueData);
         }
     }
 }

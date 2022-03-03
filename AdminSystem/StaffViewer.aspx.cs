8245c23bc45f88bb2e757e60e637e7b0ed96c4e9
Dylan Wilson <P2600429@my365.dmu.ac.uk>
2022-03-03 21:49:36 +0000

    Created the presentation layer and linked the middle layer to the data entry page

--- AdminSystem/StaffViewer.aspx.cs
+++ AdminSystem/StaffViewer.aspx.cs
@@ -4,11 +4,19 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        clsStaff AStaff = new clsStaff();
+        AStaff = (clsStaff)Session["AStaff"];
+        Session["AStaff"] = AStaff;
+        Response.Write(AStaff.StaffID);
+        Response.Write(AStaff.StaffFirstName);
+        Response.Write(AStaff.StaffLastName);
+        Response.Write(AStaff.StaffEndDate);
+        Response.Write(AStaff.StaffSalary);
     }
 }

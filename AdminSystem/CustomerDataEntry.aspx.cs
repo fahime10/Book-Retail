c04babbbc0f874f9758aa763d93ae1281e4a53cb
FabianB2002 <P2597742@my365.dmu.ac.uk>
2022-04-19 21:24:29 +0100

    Added Validation to the presentation layer

--- AdminSystem/CustomerDataEntry.aspx.cs
+++ AdminSystem/CustomerDataEntry.aspx.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Web;
+using System.Linq;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using ClassLibrary;
@@ -20,7 +20,7 @@ public partial class _1_DataEntry : System.Web.UI.Page
 
     protected void btnOK_Click(object sender, EventArgs e)
     {
-        clsCustomer TheCustomer = new clsCustomer();
+       /* clsCustomer TheCustomer = new clsCustomer();
         TheCustomer.CustomerID = Convert.ToInt32(txtCustomerID.Text);
         TheCustomer.CustomerFirstName = txtCustomerFirstName.Text;
         TheCustomer.CustomerLastName = txtCustomerLastName.Text;
@@ -28,7 +28,27 @@ public partial class _1_DataEntry : System.Web.UI.Page
         TheCustomer.CustomerAccCreated = Convert.ToDateTime(txtCustomerAccCreated.Text);
         TheCustomer.ExistingAcc = chkExistingAcc.Checked;
         Session["TheCustomer"] = TheCustomer;
-        Response.Redirect("CustomerViewer.aspx");
+        Response.Write("CustomerViewer.aspx");*/
+        clsCustomer TheCustomer = new clsCustomer();
+        string CustomerFirstName = txtCustomerFirstName.Text;
+        string CustomerLastName = txtCustomerLastName.Text;
+        string CustomerAddress = txtCustomerAddress.Text;
+        string CustomerAccountCreated = txtCustomerAccCreated.Text;
+        string Error = "";
+        Error = TheCustomer.Valid(CustomerFirstName, CustomerLastName, CustomerAddress, CustomerAccountCreated);
+        if (Error == "")
+        {
+            TheCustomer.CustomerFirstName = CustomerFirstName;
+            TheCustomer.CustomerLastName = CustomerLastName;
+            TheCustomer.CustomerAddress = CustomerAddress;
+            TheCustomer.CustomerAccCreated = Convert.ToDateTime(CustomerAccountCreated);
+            Session["TheCustomer"] = TheCustomer;
+            Response.Write("CustomerViewer.aspx");
+        }
+        else
+        {
+            lblError.Text = Error;
+        }
     }
 
     protected void btnFind_Click(object sender, EventArgs e)
